a3c9ccd0ee61cf3bfd27e479c07d261644fe81cc
Laurent tabo <talmidhaham@e-busit.com>
2019-05-12 16:24:44 +0300

    first

---
+++ src/IdentityServerAspNetIdentity/API/UserProfileController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServerAspNetIdentity.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [EnableCors("AnotherPolicy")]
+    public class UserProfileController : ControllerBase
+    {
+        // GET: api/UserProfile
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/UserProfile/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/UserProfile
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/UserProfile/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/IdentityServerAspNetIdentity/ProfileService.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Services;
+using IdentityServerAspNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class ProfileService : IProfileService
+    {
+        protected UserManager<ApplicationUser> _userManager;
+
+        public ProfileService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            //>Processing
+            var user = await _userManager.GetUserAsync(context.Subject);
+            var dbclaims = await _userManager.GetClaimsAsync(user);
+            var claims = new List<Claim>();
+            foreach (var item in dbclaims)
+            {
+                claims.Add(item);
+            }
+
+            context.IssuedClaims.AddRange(claims);
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            //>Processing
+            var user = await _userManager.GetUserAsync(context.Subject);
+
+            context.IsActive = (user != null) ;
+        }
+    }
+}
